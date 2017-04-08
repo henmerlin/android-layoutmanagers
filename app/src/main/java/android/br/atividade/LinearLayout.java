6d1a059790e7daeb1b6c2974af16fab863337ec2
henmerlin <henmerlin@gmail.com>
2017-04-08 15:28:25 -0300

    Inicio trabalho

--- app/src/main/java/android/br/atividade/LinearLayout.java
+++ app/src/main/java/android/br/atividade/LinearLayout.java
@@ -3,11 +3,11 @@ package android.br.atividade;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LinearLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_linear_layout);
     }
 }
